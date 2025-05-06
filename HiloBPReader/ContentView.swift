2aef55ad1020606d264376ab99e96e937981d6d0
ab3llini <albertomario.bellini@gmail.com>
2025-05-06 21:46:13 +0100

    Initial Commit

---
+++ HiloBPReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HiloBPReader
+//
+//  Created by Alberto Bellini on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HiloBPReader/HiloBPReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiloBPReaderApp.swift
+//  HiloBPReader
+//
+//  Created by Alberto Bellini on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HiloBPReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
